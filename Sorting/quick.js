d54bcf9bf2c4edc9fbedd36b62222fc067299b85
codewithshamim <shamimislamonline@gmail.com>
2023-03-07 14:07:48 +0600

    quick sort added

---
+++ Sorting/quick.js
@@ -0,0 +1,15 @@
+const quickSort = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  return arr;
+};
+
+console.log(quickSort([15, 2, 4, 5, 1, 3, 7, 1, 9]));
