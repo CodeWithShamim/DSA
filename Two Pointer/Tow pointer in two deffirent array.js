df947fc4c4d93e1687d241d8f1efc613cafacb29
codewithshamim <shamimislamonline@gmail.com>
2023-03-18 23:00:20 +0600

    update

--- Two Pointer/Tow pointer in two deffirent array.js
+++ Two Pointer/Tow pointer in two deffirent array.js
@@ -1,19 +1,22 @@
 function mergeSortedArrays(arr1, arr2) {
   let i = 0;
   let j = 0;
+  let k = 0;
   const mergedArray = [];
 
-  while (i < arr1.length || j < arr2.length) {
-    if (arr1[i] < arr2[j]) {
+  while (k < arr1.length + arr2.length) {
+    if (j >= arr2.length || (i < arr2.length && arr1[i] < arr2[j])) {
       mergedArray.push(arr1[i]);
       i++;
     } else {
       mergedArray.push(arr2[j]);
       j++;
     }
+
+    k++;
   }
 
   return mergedArray;
 }
 
-console.log(mergeSortedArrays([1, 2, 3, 4, 5], [1, 1, 1, 2, 2, 7, 7, 7]));
+console.log(mergeSortedArrays([1, 2], [1]));
